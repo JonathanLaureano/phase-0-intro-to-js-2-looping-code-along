8422bb183d63f3e563a74b9dde656dbbc50cc4ba
JonathanLaureano <JonathanLaureano20@gmail.com>
2021-11-22 05:56:07 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cards = [];
+const cardArr = [];
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    cardArr.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+  }
+
+  return cardArr;
+}
+
+writeCards(cards);
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--)
+    }
+}
